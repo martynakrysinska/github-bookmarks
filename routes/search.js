77b1658896c3967a2139fc56d60dbda59ab24f47
martynakrysinska <mlecharka@gmail.com>
2019-11-16 14:43:14 +0100

    connected search controller to the middleware

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const searchRouter = express.Router()
-
+const sendJson = require("../middleware/sendJson")
+const sendError = require("../middleware/sendError")
 // In this case there's only one route and one controller for searching through github repositories,
 // but with this structure it's easy to scale up and add more features that Github's API provides
 
@@ -8,4 +9,7 @@ const { searchByKeyword } = require("../controllers/search")
 
 searchRouter.get("/", searchByKeyword)
 
+// middleware
+searchRouter.use(sendJson)
+searchRouter.use(sendError)
 module.exports = { searchRouter }
