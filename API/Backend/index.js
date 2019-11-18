73cf48e9df60b4bc8747f801c9905e8255d224f4
martynakrysinska <mlecharka@gmail.com>
2019-11-18 22:09:29 +0100

    Enabled cors on the backend

--- API/Backend/index.js
+++ API/Backend/index.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const logger = require("morgan")
+const cors = require("cors")
 
 // Routers:
 const { bookmarksRouter } = require("./routes/bookmarks")
 const { searchRouter } = require("./routes/search")
 
 const app = express()
+
 app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 app.use("/bookmarks", bookmarksRouter)
 app.use("/search", searchRouter)
